6cbc3ce0c180b58b8fd1856321cc5edd21d8a324
hokton-bd <skmhkt.p@gmail.com>
2020-10-22 11:33:53 +0900

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,5 +37,26 @@ $(document).ready(function() {
         toggleChangeBtn();
     });
 
+    $('.opener').on('click', function() {
 
-});
+        $('.drawer').addClass('d-check');
+
+    });
+
+    $('.closer').on('click', function() {
+
+        $('.drawer').removeClass('d-check');
+
+    });
+
+    $('a[href^="#"]').click(function(){
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+      });
+
+
+});//document ready
